3f86198daa6f3e2d16a63db14774085ed9fa3748
Gouri Matta <4967769+Lexmark-shmatta@users.noreply.github.com>
2025-11-06 11:16:52 -0500

    Initialize b2b-po-integration

---
+++ scripts/__dropins__/storefront-purchase-order/mocks/storybook/events-data.d.ts
@@ -0,0 +1,8 @@
+export declare const mockPermissions: {
+    [x: string]: boolean;
+    all: boolean;
+    'Magento_Company::index': boolean;
+    'Magento_Sales::all': boolean;
+    'Magento_Company::user_management': boolean;
+};
+//# sourceMappingURL=events-data.d.ts.map
